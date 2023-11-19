e47376694c0214b47d04f44041147d0ab880eceb
binarypsilocybin <rogeriosvaldo@gmail.com>
2023-11-19 15:23:29 +0000

    added new fetch for jobs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import backgroundHeader from "./assets/bg-header-desktop.svg";
 
+import { useState, useEffect } from "react";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const handleFetchData = async () => {
+      try {
+        const res = await fetch("./data.json");
+        const dataRes = await res.json();
+        setData(dataRes);
+      } catch (error) {
+        setIsError(true);
+        console.log(error);
+      }
+    };
+    handleFetchData();
+  }, []);
+
+  console.log(data);
+
   return (
-    <>
+    <main>
       <header>
         <img src={backgroundHeader} alt="background-header-pattern" />
       </header>
@@ -11,7 +34,7 @@ function App() {
           <div className="job-card"></div>
         </div>
       </div>
-    </>
+    </main>
   );
 }
 
