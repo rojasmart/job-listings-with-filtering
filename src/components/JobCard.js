12700530b053269b2d29acd3a8a55c8301dc9cf1
binarypsilocybin <rogeriosvaldo@gmail.com>
2023-11-23 22:29:11 +0000

    Added chips for filters layout

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -4,7 +4,7 @@ const JobCard = ({ item }) => {
       <img src={item.logo} alt="job-avatar" />
       <div className="job-info">
         <p>{item.company}</p>
-        <p>{item.position}</p>
+        <h3>{item.position}</h3>
         <div className="job-info-secondary">
           <p>{item.postedAt}</p>
           <p>{item.contract}</p>
@@ -14,8 +14,10 @@ const JobCard = ({ item }) => {
       <div className="job-categories">
         <p>{item.role}</p>
         <p>{item.level}</p>
-        <p>{item.languages}</p>
-        <p>{item.tools}</p>
+        {item.languages.map((language) => {
+          return <span>{language}</span>;
+        })}
+        {item.tools.length > 0 && <span>{item.tools}</span>}
       </div>
     </div>
   );
